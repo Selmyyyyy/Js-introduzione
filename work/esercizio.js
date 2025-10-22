247c24e306d831a64ebc30d1e299c4c6b8b31548
Selmyyyyy <salmasobhi2200@gmail.com>
2025-10-22 09:59:53 +0200

    esercizi js

---
+++ work/esercizio.js
@@ -0,0 +1,66 @@
+
+// let firstName = prompt("Come ti chiami?");
+// alert("Buon compleanno, " + firstName + "! ");
+
+// let base = parseFloat(prompt("Inserisci la base:"));
+// let esponente = parseFloat(prompt("Inserisci l'esponente:"));
+// let potenza = Math.pow(base, esponente);
+// alert(`${base} elevato a ${esponente} è ${potenza}`);
+
+// let Number = parseInt(prompt("Inserisci un numero intero:"));
+// if ( Number % 2 === 0) {
+//   alert("Il numero è pari.");
+// } else {
+//   alert("Il numero è dispari.");
+// }
+
+// let age = parseInt(prompt("Quanti anni hai?"));
+// if (age >= 18) {
+//   alert("Puoi entrare.");
+// } else {
+//   alert("Mi dispiace, non puoi entrare.");
+// }
+
+
+// let stringa = prompt("Inserisci una frase o parola:");
+// let maiuscola = stringa.toUpperCase();
+// alert("In maiuscolo: " + maiuscola);
+   
+// 1)
+// const firstName = prompt("Come ti chiami?");
+// alert("Buon compleanno, " + firstName + "! ");
+
+// 2)
+// const number1 = prompt("Dammi le base della potenza");
+// const number2 = prompt("Dammi l'esponente");
+
+// const n1converted = Number(number1);
+// const n2converted = Number(number2);
+
+// const result = number1**number2;
+// alert("il risultato è: "+result)
+
+// 3)
+// const numb = prompt("Dammi un numero");
+// if (Number % 2 === 0){
+// alert("il numero è pari");
+// } else{
+//     alert("il numero è dispari");
+// }
+
+// 4)
+
+// const age = prompt("dammi l'età");
+
+// if( age >= 18){
+//     alert("puoi entrare");
+// }else{
+//     alert("non puoi entrare");
+// }
+
+// 5)
+//  const string = prompt("dammi una parola o una frase");
+//  const maiuscola =  string.toUpperCase();
+//  alert("in maiuscolo:" + maiuscola);
+
+// cerca di usare const invece di let
